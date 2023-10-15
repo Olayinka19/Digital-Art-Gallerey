ccc4795e2f65a1af9cdd9fd7b348379b8745056e
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2023-10-15 08:33:21 -0400

    Digital art gallery

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -4,9 +4,11 @@ import ArtPiece from './ArtPiece';
 function Gallery({ artPieces }) {
   return (
     <div className="gallery">
+    
       {artPieces.map((art, index) => (
         <ArtPiece key={index} {...art} />
       ))}
+      <iframe src="https://chitchatter.im/public/886345ce-9ded-4c3e-a259-bb2d5596df8a?embed=1" allow="camera;microphone;display-capture;fullscreen" width="800" height="800" />
       <h1 className='gallery-art'>Art Gallery Project By: Kehinde Oladipupo</h1></div>
    
   );
